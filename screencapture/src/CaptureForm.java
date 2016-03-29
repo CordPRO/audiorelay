fcf0ae5e730bf34a802f4739a37efee4594aa1f1
PD(MWPP)-CEB <PD(MWPP)-CEB>
2016-03-29 23:20:24 +0530

    Multiplexer Implemented

--- screencapture/src/CaptureForm.java
+++ screencapture/src/CaptureForm.java
@@ -4,8 +4,7 @@ import com.renovelabz.server.FrameBufferImplService;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -20,29 +19,47 @@ public class CaptureForm extends JFrame {
     private JPanel jPanel1;
     private JButton button1;
     private JLabel jImageLabel;
-    AudioCapturer cap;
-    AudioReceiver recv;
+    private JButton btnSwitch;
+    ScreenCapturer cap;
+    ScreenCapturer cap2;
+    ScreenReceiver recv;
+
+    private boolean channel1 = false;
+    Multiplexer multi;
     public CaptureForm()
     {
+        FrameBufferImplService service2 = null;
         FrameBufferImplService service = null;
+        FrameBufferImplService serviceOutput = null;
         try {
-            service = new FrameBufferImplService(new URL("http://localhost:8000/audiorelay?wsdl"));
+            service = new FrameBufferImplService(new URL("http://localhost:8000/screenrelay?wsdl"));
+            service2 = new FrameBufferImplService(new URL("http://localhost:8000/screenrelay2?wsdl"));
+            serviceOutput = new FrameBufferImplService(new URL("http://localhost:8000/screenrelayoutput?wsdl"));
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
 
+        try {
+            multi = new Multiplexer(new URL("http://localhost:8000/screenrelayoutput?wsdl"));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        FrameBuffer outputBuffer = serviceOutput.getFrameBufferImplPort();
+        FrameBuffer frameBuffer2 = service2.getFrameBufferImplPort();
         FrameBuffer frameBuffer = service.getFrameBufferImplPort();
 
-        cap = new AudioCapturer(frameBuffer);
+        cap = new ScreenCapturer(frameBuffer,1000);
+        cap2 = new ScreenCapturer(frameBuffer2,100);
 
-        recv = new AudioReceiver(frameBuffer);
+        recv = new ScreenReceiver(outputBuffer,50);
 
-        /*recv.addScreenReceivedListener(new ScreenReceiver.ScreenReceivedListener() {
+        recv.addScreenReceivedListener(new ScreenReceiver.ScreenReceivedListener() {
             @Override
             public void ScreenReceived(byte[] screen, BufferedImage screenImage) {
                 jImageLabel.setIcon(new ImageIcon(screenImage));
             }
-        });*/
+        });
 
         recv.StartReceiving();
 
@@ -50,13 +67,53 @@ public class CaptureForm extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
 
-                if(cap.IsCapturing() )
+                if(cap2.IsCapturing() )
+                {
                     cap.StopCapturing();
-                else
+                    cap2.StopCapturing();
+                }
+                else {
                     cap.StartCapture();
+                    cap2.StartCapture();
+                    multi.Start();
+                }
             }
         });
 
+        btnSwitch.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(channel1)
+                {
+                    channel1 = false;
+                    try {
+                        setTitle("Channel 2");
+                        multi.setInputURL(new URL("http://localhost:8000/screenrelay2?wsdl"));
+                    } catch (MalformedURLException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+                else
+                {
+                    channel1 = true;
+                    try {
+                        setTitle("Channel 1");
+                        multi.setInputURL(new URL("http://localhost:8000/screenrelay1?wsdl"));
+                    } catch (MalformedURLException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+
+            }
+        });
+
+        jImageLabel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                super.mousePressed(e);
+                btnSwitch.doClick();
+            }
+        });
 
     }
 
---
+++ screencapture/src/Multiplexer.java
@@ -0,0 +1,123 @@
+import com.renovelabz.server.FrameBuffer;
+import com.renovelabz.server.FrameBufferImplService;
+import com.renovelabz.server.FramePullResult;
+
+import java.net.URL;
+
+/**
+ * Created by Fujitsu on 3/29/2016.
+ */
+public class Multiplexer {
+    private URL outputURL;
+    private URL inputURL = null;
+    private boolean running = false;
+    private Thread thread = null;
+    private boolean switchPending = false;
+
+    public Multiplexer(URL outputURL)
+    {
+        this.outputURL = outputURL;
+    }
+
+    public synchronized URL getOutputURL() {
+        return outputURL;
+    }
+
+    private synchronized void markSwitchPending()
+    {
+        switchPending = true;
+    }
+    private synchronized void clearSwitchPending()
+    {
+        switchPending = false;
+    }
+    private synchronized boolean isSwitchPending()
+    {
+        return switchPending;
+    }
+
+    public synchronized URL getInputURL() {
+        return inputURL;
+    }
+
+    public synchronized void setInputURL(URL inputURL) {
+        this.inputURL = inputURL;
+        markSwitchPending();
+    }
+
+    public synchronized boolean isRunning() {
+        return running;
+    }
+
+    private synchronized void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    public synchronized void Stop()
+    {
+        setRunning(false);
+    }
+
+    public synchronized void Start()
+    {
+        if(isRunning())
+            return;
+
+        //if an input url is already assigned, mark that a new switch is pending. this will ensure that start new segment is called in output stream
+        if(getInputURL() != null)
+            markSwitchPending();
+
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //open output connection
+                FrameBufferImplService outputService = new FrameBufferImplService(getOutputURL());
+                FrameBuffer outputBuffer = outputService.getFrameBufferImplPort();
+
+                FrameBufferImplService inputService = null;
+                FrameBuffer inputBuffer = null;
+
+                int segmentID = 0;
+                int nextFrameID = 0;
+
+                while(isRunning())
+                {
+                    if(isSwitchPending())
+                    {
+                        //a switch is pending, implement  it
+                        inputService = new FrameBufferImplService(getInputURL());
+                        inputBuffer = inputService.getFrameBufferImplPort();
+
+                        segmentID = 0;
+                        nextFrameID = 0;
+
+                        outputBuffer.startNewSegment();
+                        clearSwitchPending();
+                    }
+                    else
+                    {
+                        //tunnel input to output
+                        if(inputBuffer != null)
+                        {
+                            FramePullResult pullResult = inputBuffer.pullFrames(segmentID,nextFrameID);
+                            segmentID = pullResult.getSegmentIndex();
+                            nextFrameID = pullResult.getNextFrameId();
+                            for(byte[] block : pullResult.getData())
+                            {
+                                outputBuffer.pushFrame(block);
+                            }
+                        }
+                    }
+                }
+
+                inputBuffer = null;
+                inputService = null;
+                outputBuffer = null;
+                outputService = null;
+            }
+        });
+
+        setRunning(true);
+        thread.start();
+    }
+}
